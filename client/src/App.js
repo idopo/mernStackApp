a11a6a9280c3b12d8071191b5f989da889937b8e
idopo <idoaomer@gmail.com>
2019-06-05 16:27:59 +0300

    redux part 1

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavbar from "./Components/AppNavbar";
 import ShoppingList from './Components/ShoppingList';
+import {Provider} from 'react-redux';
+import store from './store'
 
 class  App extends Component {
   render() { return(
-    <div className="App">
-      <AppNavbar/>
-      <ShoppingList/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar/>
+        <ShoppingList/>
+      </div>
+    </Provider>
   );
   }
 }
--- client/src/Components/ShoppingList.js
+++ client/src/Components/ShoppingList.js
@@ -2,26 +2,23 @@ import React, {Component} from 'react';
 import {Container,ListGroup ,ListGroupItem,Button} from 'reactstrap';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import uuid from 'uuid';
+import {connect} from 'react-redux';
+import {getItems} from '../actions/itemActions';
+import PropTypes from 'prop-types';
+
 
 class ShoppingList extends Component{
-
-    state={
-        items: [
-            {id: uuid() , name: "honey" },
-            {id: uuid() , name: "salt" },
-            {id: uuid() , name: "sugar" },
-            {id: uuid() , name: "lemons" }
-
-        ]
+    componentDidMount(){
+        this.props.getItems();
     }
-    
+   
     render(){
-        const {items} = this.state;
+        const {items} = this.props.item;
         return(
             <Container>
                 <Button 
                 color = "dark" 
-                style={{scrollMarginBottom: "2rem"}} 
+                style={{margin: "2rem 0 "}} 
                 onClick={ ()=>{
                     const name = prompt('Enter Item');
                     if(name){
@@ -38,6 +35,25 @@ class ShoppingList extends Component{
                         {items.map(( {id ,name} )=> (
                             <CSSTransition key={id} timeout={500} classNames="fade">
                                 <ListGroupItem>
+                                    <Button
+                                        className= 'remove-btn'
+                                        color='danger'
+                                        size='sm'
+                                        onClick= {
+                                            ()=> this.setState(state =>({
+                                                items: state.items.filter(item=> item.id!==id)
+                                            }))
+                                        }
+                                    
+                                    
+                                    
+                                    
+                                    >
+                                        &times;
+
+
+
+                                    </Button>
                                     {name}
                                 </ListGroupItem>
                             </CSSTransition>
@@ -50,5 +66,13 @@ class ShoppingList extends Component{
 
 }
 
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
 
-export default ShoppingList;
+const mapStateToProps = (state)=> ({
+    item:state.item
+});
+
+export default connect(mapStateToProps,{getItems})(ShoppingList);
